ce5200c9ce9e4c396d175124c9d33630a90630b6
Zouhair <zamazzal@student.1337.ma>
2020-01-24 21:30:57 +0100

    clean

--- ZAMAZZAL_LASTWORK/zzzzzz/readline/run.sh
+++
@@ -1,1 +0,0 @@
-gcc main.c readline/* vars/* libft/*.c -I libft -I readline -I . -ltermcap -o 21sh
--- ZAMAZZAL_LASTWORK/zzzzzz/run.sh
+++
@@ -1,1 +0,0 @@
-gcc main.c execution/*.c parsing/* readline/readline/* libft/*.c readline/libft/*.c -I includes -I readline -I readline/libft -ltermcap readline/vars/*.c -o 42sh
